0995ef513e1d9f87e765f03bc9ac7f699b698449
kenwood chan <kenwoodchan@kenwooddeMacBook-Pro.local>
2023-06-19 17:42:24 +0800

    init
    Signed-off-by: kenwood chan <kenwoodchan@kenwooddeMacBook-Pro.local>

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div id="app">
+    <!-- 将 <n-message-provider> 包裹在根组件中 -->
+    <n-message-provider>
+      <router-view />
+    </n-message-provider>
+  </div>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div> -->
+  </header>
+
+  <RouterView />
+</template>
+
+
+
